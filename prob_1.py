08036c0c096027f07d856106e3eff50b4c296fb7
francrispin6 <francrispin6@gmail.com>
2016-05-31 13:17:06 -0600

    subi prob_1

---
+++ prob_1.py
@@ -0,0 +1,7 @@
+#Proyecto numero 1
+
+def prob_1(a):
+	if a % 2 == 0:
+		return("El numero es par")
+	else:
+		return("El numero es impar")
---
+++ prob_4.py
@@ -0,0 +1,6 @@
+#Proyecto numero 4
+def prob_4(n, p):
+	l = len(p)	
+	cas=(n-l)/2
+
+return(cas)
