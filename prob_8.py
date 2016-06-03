f70a44b99d01be3ad0b77792cd059893de5a53cb
walterzea <walterzearojas@hotmail.com>
2016-06-03 07:52:45 -0600

    Proyecto 8

---
+++ prob_8.py
@@ -0,0 +1,7 @@
+def prob_8(numero_linea):
+	for numero_linea in range(lineas): 
+		espacios = lineas - numero_linea - 1 
+		asteriscos = 1 + numero_linea * 2 
+	a=" " * espacios + "*" * asteriscos
+
+return(a)
