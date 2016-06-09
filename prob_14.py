9e5d74bbf6dea2a5df75e79cecfa20d3fdb2bd67
walterzea <walterzearojas@hotmail.com>
2016-06-09 07:19:16 -0600

    problema14

---
+++ prob_14.py
@@ -0,0 +1,11 @@
+def prob_14:
+	cont=0
+	suma=0
+	cont2=0 el dato
+		divisor=numero/cont 
+		if divisor.is_integer() == True:
+			cont2+=1
+	if cont2==2 or cont2==1:
+		print("es primo")
+	else:
+		print("no es primo")
